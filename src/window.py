ddc9a9bdec5e2c8a9907040649995d25b5aa3f3e
navivan123 <ivanfran49er@gmail.com>
2024-08-14 16:30:19 -0400

    Initial Commit. Made dummy main and the Window class to display app using tkinter.

---
+++ src/window.py
@@ -0,0 +1,34 @@
+from tkinter import Tk, BOTH, Canvas
+
+class Window:
+
+    
+    def __init__(self, width, height):
+        self.__root_wdgt = Tk()
+        self.__root_wdgt.title("Maze Solver!")
+        
+        self.__canvas_wdgt = Canvas(self.__root_wdgt, bg="white", height=height, width=width)
+
+        self.__canvas_wdgt.pack(fill=BOTH, expand=1)
+
+        self.__is_window_running = False
+
+        self.__root_wdgt.protocol("WM_DELETE_WINDOW", self.close)
+
+    
+    def redraw(self):
+        self.__root_wdgt.update_idletasks()
+        self.__root_wdgt.update()
+
+    
+    def wait_for_close(self):
+        self.__is_window_running = True
+
+        while self.__is_window_running:
+            self.redraw()
+        
+        print("Wow, the window closed!")
+
+
+    def close(self):
+        self.__is_window_running = False
